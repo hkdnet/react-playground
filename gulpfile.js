ce36fe0a9166c3e4ff0718fb77ca27efba1831e9
hkdnet <hkdnet@users.noreply.github.com>
2015-03-20 01:24:12 +0900

    add db, gulp tasks for build and so on

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require("vinyl-source-stream");
+var reactify = require('reactify');
 
 gulp.task('build.copy', function() {
   gulp.src('src/javascripts/*.js')
@@ -9,6 +12,23 @@ gulp.task('build.copy', function() {
     .pipe(gulp.dest('public/stylesheets'));
 });
 
+gulp.task('browserify', function(){
+  var b = browserify({
+    entries: ['./src/javascripts/hello.jsx'],
+    transform: [reactify]
+  });
+  return b.bundle()
+    .pipe(source('hello.js'))
+    .pipe(gulp.dest('./public/javascripts'));
+});
+
 gulp.task('build', function() {
   gulp.run('build.copy');
+  gulp.run('browserify');
 })
+
+gulp.task('run', function() {
+  gulp.run('build');
+  var exec = require("child_process").exec
+  exec('json-server db.json')
+});
