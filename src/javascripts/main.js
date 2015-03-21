7961b6af7625a7d8cb5efffd4e83e617358c3ff9
hkdnet <hkdnet@users.noreply.github.com>
2015-03-21 14:08:41 +0900

    add CommentList and CommentForm

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,34 @@
 var React = require('React');
-var CommentBox = React.createClass({
-  displayName: 'CommentBox',
+
+var CommentList = React.createClass({
   render: function() {
     return (
-      React.createElement('div', {className: "commentBox"},
-        "Hello, world! I am a CommentBox."
-      )
-    );
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    )
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    )
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    )
   }
 })
 
